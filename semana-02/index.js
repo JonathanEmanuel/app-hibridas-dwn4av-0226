d90a55218ec8bf059716c3a4505b616f5b2b601c
Jonathan Cruz <hola@jonathancruz.com.ar>
2026-08-18 22:49:06 -0300

    Semana 02 - Servidor Web con Express.js

--- semana-02/index.js
+++ semana-02/index.js
@@ -9,7 +9,33 @@ let count = 0;
 app.get('/', (request, response) => {
     count++;
     console.log(`Cliente conectado N: ${count}`);
-    response.send(`Hola desde Express.js👋 Sos el cliente n ${count}`);
+    response.send(`<h1>Hola desde Express.js👋</h1> 
+                    <p style='color: teal'>Sos el cliente número ${count} </p>
+                    <ul>
+                        <li><a href='/subjects'> Listado de materias </a> </li>
+                        <li><a href='/register'> Registro </a> </li>
+                        <li><a href='/contact'> Contactos </a> </li>
+                    </ul>`);
+})
+
+app.get('/subjects', (request, response) => {
+    console.log('GET sobre Materias');
+    response.send('<h1>Listado de materias</h1>');
+})
+
+const register = (request, response) => {
+    response.send('<h1>Página de registro de usuario</h1>');
+}
+
+app.get('/register', register);
+
+app.get('/api/users', (request, response) => {
+    const data = gestor.getUsers();
+    response.json(data);
+})
+
+app.post('/api/users', (request, response) => {
+    response.send('Usuario Registrado');
 })
 
 console.log(chalk.blue.bgRed.bold('Hello world!'));
