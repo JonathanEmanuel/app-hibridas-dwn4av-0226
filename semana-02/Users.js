2906a8600f2160e8885e8470ce9f3b6f5308134f
Jonathan Cruz <hola@jonathancruz.com.ar>
2026-08-20 19:17:39 -0300

    Semana 02 - Servidor con Express

--- semana-02/Users.js
+++ semana-02/Users.js
@@ -21,6 +21,37 @@ class Users {
     getUsers(){
         return this.users;
     }
+    getUserById(id){
+        const user = this.users.find( u => u.id === id);
+        return user;
+    }
+    updateUser(id, user){
+        const index = this.users.findIndex( u => u.id === id);
+        if( index == -1){
+            return 'Not Found';
+        }
+
+        if( user.name){
+            this.users[index].name = user.name;
+        }
+        if( user.email){
+            this.users[index].email = user.email;
+        }
+
+        if( user.password){
+            this.users[index].password = user.password;
+        }
+
+        this.saveUsers()
+    }
+    deleteUserById(id){
+        const index = this.users.findIndex( u => u.id === id);
+        if( index == -1){
+            return 'Not Found';
+        }
+        this.users.splice(index, 1);
+        return 'ok';
+    }
     saveUsers(){
         const data = JSON.stringify(  this.users, null, 2 );
         fs.writeFileSync(this.path, data, 'utf-8');
